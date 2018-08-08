f48710b30d77b31ed5b0a459506be8087188c13f
fjz <fjz0121@163.com>
2018-08-08 17:26:09 +0800

    'add-zhushi'

--- js/componentBase.js
+++ js/componentBase.js
@@ -3,51 +3,76 @@
 * @Author: fjz
 * @Date:   2018-08-06 16:46:58
 * @Last Modified by:   fjz
-* @Last Modified time: 2018-08-08 12:26:29
+* @Last Modified time: 2018-08-08 17:04:16
 */
+// 这里主要规定了组件的元素、id、class、width、height、css以及组件的载入载出
 var componentBase = function(name,config) {
-	var config = config || {},
-		id = ('h5_' + Math.random()).replace('.', '_'),
-		cls = 'h5_component_' + config.type + ' h5_component_name_' + name;
 
-	var component = $('<div class="h5_component '+ cls+'" id="' + id +'">');
-	
+	var config = config || {},
+		id = ('h5_' + Math.random()).replace('.', '_'),//创建组件的唯一id值
+		cls = 'h5_component_' + config.type + ' h5_component_name_' + name;//根据传递进来的name，创建组件的classname
+
+
+	// 创建组件
+	var component = $('<div class="h5_component '+ cls +'" id="' + id +'">');
+	// 如果组件存在text则给该组件添加一个text
 	config.text && component.text(config.text);
+	// 如果组件传入时事先预定了宽高的值，则把该值赋予组件
 	config.width && component.width(config.width/2);
 	config.height && component.height(config.height/2);
+	// css也与上面的内容一样
 	config.css && component.css(config.css)
-
+	// 将传入的背景图信息赋予css背景
 	config.bg && component.css('backgroundImage', 'url('+ config.bg +')');
 
+	// 如果组件传入了center参数，则将组件水平居中
 	if (config.center === true) {
 		component.css({
+			// 这里之所以除以4是因为在高清屏幕上，一个元素是当做两个元素使用的
+
 			marginLeft: (config.width/4 * -1) + 'px',
 			left: '50%'
 		})
 	}
 
+	// 如果传入了onclick参数，则在点击这个组件的时候出发传入时的事件
+	// 这个事件其实只有最后一个页面的back组件才有用到
+	// 这是用jquery的on事件实现的
 	if (typeof config.onclick === 'function') {
 		component.on('click',config.onclick)
 	}
 
+
+	// 这里的onload事件时自定义的
+	// 也就是说需要靠this.onLeave事件去触发，而不能自动执行
 	component.on('onLoad', function(){
 		setTimeout(function() {
-			component.addClass(cls + ' _load').removeClass(' _leave');
+
+			// 组件被载入的时候给组件添加_load类并且同时移除_leave类
+			component.addClass(' _load').removeClass(' _leave');
+
+			// 组件被载入的时候触发组件的动画函数（这是用jquery的animate实现的）
 			config.animateIn && component.animate(config.animateIn);
+
 		},config.delay || 0)
 
 		return false;
 	});
+
 	
 	component.on('onLeave',function(){
 
 		setTimeout( function () {
-			component.addClass(cls + ' _leave').removeClass(' _load');
+
+			component.addClass(' _leave').removeClass(' _load');
+
 			config.animateOut && component.animate(config.animateOut);
+
 		}, config.delay || 0)
 		
 		return false;
 	});
+
 	
 
 	return component;
--- js/test_h5.js
+++ js/test_h5.js
@@ -2,41 +2,67 @@
 * @Author: fjz
 * @Date:   2018-08-06 18:58:10
 * @Last Modified by:   fjz
-* @Last Modified time: 2018-08-08 14:15:19
+* @Last Modified time: 2018-08-08 17:07:28
 */
 var H5 = function () {
+
+	//创建根部的包裹元素的id
 	this.id = ('h5_' + Math.random()).replace('.','_');
-	this.el = $('<div class="h5" id="'+this.id+'">').hide();
+	//创建根部的包裹元素
+	this.el = $('<div class="h5" id="'+ this.id +'">').hide();
+
+
 	this.page = [];
 
+	//把包裹元素添加至body内
 	$('body').append(this.el);
+
 	// 新增一个页
 	this.addPage = function (name,text) {
+
+		// 创建这个页元素，并赋予其section类，以作用于fullpage
 		var page = $('<div class="h5_page section">');
 
+		//只有首页才出现了name，作用是为首页添加一个页面背景图片
 		if (name != undefined) {
 			page.addClass('h5_page_' + name)
 		}
+
+		// 所有页面都没有添加过这个text，但为了保险起见，
+		// 事先在h5这里先把逻辑写出来
 		if (text != undefined) {
 			page.text(text)
 		}
+
+		// 把新增的页面添加至根部的包裹元素中
 		this.el.append(page);
+
+		// 然后把新增的页面存储进一个数组
 		this.page.push(page)
+
+		// 这里的作用：响应index.html中的添加footer。
+		// 如果这个函数存在，则触发index.html中的whenAddPage()函数
 		if (typeof this.whenAddPage === 'function') {
 			this.whenAddPage()
 		}
 		return this;
 	};
+
 	//新增一个组件
 	this.addComponent = function(name,config) {
+
+
 		var config = config || {},
 			component,
 			page = this.page.slice(-1)[0];
+
 			config = $.extend({
-			type : 'base'
-		}, config);
+				type : 'base'
+			}, config);
 
 		switch(config.type) {
+			// 判断传入的组件数据的类型，给对应的类型寻找对应的组件
+			// 并且把组件所需要的name和config传递进来
 			case 'base':
 				component = new componentBase(name,config);//将componentBase组件中的数据传递进来
 			break;
@@ -63,17 +89,27 @@ var H5 = function () {
 
 		return this;
 	}
+	// 只要增加一个页面便可以触发这个事件
 	this.loader = function(firstPage) {
+
 		this.el.fullpage({
-			onLeave: function(index, nextIndex, direction) {
+			// 页面滚动的时候触发组件的载入事件
+			// 也就是说包裹根元素载入的时候，组件同时跟着载入，所以需要触发载入事件
+			onLeave: function(index, nextIndex, direction) { //页面消失
 			$(this).find('.h5_component').trigger('onLeave');
 			},
-			afterLoad: function(anchorLink, index) {
+
+			afterLoad: function(anchorLink, index) { // 页面载入
 			$(this).find('.h5_component').trigger('onLoad');
 			}
 		})
+
+		// 页面刷新的时候自动载入组件
 		this.page[0].find('.h5_component').trigger('onLoad');
+
+		// 将根元素显示
 		this.el.show()
+
 		if (firstPage) {
 			$.fn.fullpage.moveTo(firstPage)
 		}
